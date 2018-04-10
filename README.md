e77a01b9e04911da89d1c1d068792bda8550b1c8
Ashish Singh <ashishcodes4@gmail.com>
2018-04-10 08:04:43 +0530

    converted app.js to index.js

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Day-1: {
     - created first component
     - ceated a new youtube search API
     - NPM package for youtube-api-search added to the node modules in locally and also to the package.json file.
+
+
+    STATE: State is nothing but a javaScript object which is used to keep a track and record USER EVENTS.
+    
+    DOWNWARDS DATA FLOW: The most parent component is always responsible for fetching the data.
 }
 
 Author : ashischcodes4.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.js';
+import YTsearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAiD8VI8ooPXTGrwCVU4oJ5OMLwgKlBqj4';
 
 
-const API_KEY = 'AIzaSyDgLXWOnihs40bAqMvT2NmETGHHiHC4DrM';
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        }
+
+        YTsearch({key: API_KEY, term: 'reactjs'}, function(data){
+            console.log(data);
+          })
+    }
+  render() {
+    return (
+      <div>
+        <h1>React simple starter</h1>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
