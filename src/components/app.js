7e0597101908d4f43fe069267654f078581c22da
Ashish Singh <ashishcodes4@gmail.com>
2018-04-10 07:13:33 +0530

    component added

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import test from './test';
+import SearchBar from './search_bar';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
         <h1>React simple starter</h1>
-        <test />
+        <SearchBar />
       </div>
     );
   }
 }
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/components/app.js';
+import App from './components/app.js';
 
+
+const API_KEY = 'AIzaSyDgLXWOnihs40bAqMvT2NmETGHHiHC4DrM';
 
 ReactDOM.render(<App />, document.querySelector('.container'));
